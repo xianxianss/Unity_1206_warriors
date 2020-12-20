261858e441cf4b877aa270969701bed20756ac20
xianxianss <75553503+xianxianss@users.noreply.github.com>
2020-12-20 15:57:38 +0800

    語法和欄位練習

--- 201206_Warriors/Assets/Scripts/Car.cs
+++ 201206_Warriors/Assets/Scripts/Car.cs
@@ -1,18 +1,35 @@
-﻿using System.Collections;
-using System.Collections.Generic;
+﻿
 using UnityEngine;
 
 public class Car : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+    [Header("這是汽車的高度")]
+    public int height = 3;
+    [Tooltip("這是汽車的重量，單位是噸") , Range(1.5f,10)]
+    public float weight = 6.5f;
+    public string brand = "BMW";
+    public bool hasWindows = true;
+    [Header("顏色選擇")]
+    public Color color1 = new Color(0.1f, 5, 4.2f);
+    public Color color2 = Color.yellow;
+    public Color color3;
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    [Header("座標")]
+    public Vector2 v2no1 = Vector2.zero;
+    public Vector2 v2no2 = Vector2.one;
+    public Vector2 v2no3 = new Vector2(12, 7);
+    public Vector3 v3no1 = new Vector3(89, 12, 7);
+    public Vector4 v4no1 = new Vector4(19, 89, 12, 7);
+
+    [Header("圖片與音效")]
+    public Sprite picture1;
+    public AudioClip sound1;
+
+    [Header("遊戲物件與元件")]
+    public GameObject objt1;
+    public GameObject objt2;
+
+    public Transform tras1;
+    public Camera cam1;
+
 }
