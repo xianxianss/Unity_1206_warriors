d29f2d02d5708061907038dbeaa25840dc29d3bc
xianxianss <75553503+xianxianss@users.noreply.github.com>
2020-12-27 13:40:47 +0800

    方法與事件

--- 201206_Warriors/Assets/Scripts/Car.cs
+++ 201206_Warriors/Assets/Scripts/Car.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 
 public class Car : MonoBehaviour
 {
+    #region 練習欄位
     [Header("這是汽車的高度")]
     public int height = 3;
     [Tooltip("這是汽車的重量，單位是噸") , Range(1.5f,10)]
@@ -31,5 +32,79 @@ public class Car : MonoBehaviour
 
     public Transform tras1;
     public Camera cam1;
+    #endregion
 
+    #region 練習方法
+    private void Test()
+    {
+        print("哈囉");
+    }
+
+    private void Start()
+    {
+        Test();
+
+        print("品牌:" + brand);
+        print("高度:" + height);
+        print("顏色:" + color1);
+
+        hasWindows = false;
+        weight = 8.6f;
+
+        print("傳回的數字:"+five());
+        print("傳回的數字:" +sixsix());
+
+        string myName = Name();
+        print(myName);
+
+        Drive("後面","轟轟轟",200);
+        Drive("前面","轟轟轟",120);
+        Drive("左邊","轟轟轟",150);
+
+        OpenBrush("刷刷",speed:500);
+
+    }
+
+    private int five()
+    {
+        return 5;
+    }
+
+    private float sixsix()
+    {
+        return 6.6f;
+    }
+
+    private string Name()
+    {
+        return "xianxian";
+    }
+
+    /// <summary>
+    /// 開車方法
+    /// </summary>
+    /// <param name="direction">方向</param>
+    /// <param name="sound">音效</param>
+    /// <param name="speed">速度</param>
+    private void Drive (string direction,string sound,int speed)
+    {
+        print("開車方向:" + direction);
+        print("開車音效:"+ sound);
+        print("開車速度:" + speed);
+    }
+
+    /// <summary>
+    /// 開啟雨刷
+    /// </summary>
+    /// <param name="speed">雨刷速度</param>
+    private void OpenBrush(string sound,int count=20, int speed = 148)
+    {
+        print("開雨刷");
+        print("雨刷音效:" + sound);
+        print("雨刷速度:"+speed);
+        print("雨刷數量:"+count);
+    }
+
+
+    #endregion 
 }
--- 201206_Warriors/Assets/Scripts/Player.cs
+++ 201206_Warriors/Assets/Scripts/Player.cs
@@ -4,7 +4,8 @@ using UnityEngine;
 
 public class Player : MonoBehaviour
 {
-    [Header("移動速度") , Range(0,1000)]
+    #region 欄位
+    [Header("移動速度"), Range(0, 1000)]
     public float speed = 10.5f;
 
     [Header("跳躍高度"), Range(0, 3000)]
@@ -13,19 +14,55 @@ public class Player : MonoBehaviour
     [Header("是否在地板上"), Tooltip("是否在地板上")]
     public bool onFloor = false;
 
-    [Header("子彈"), Tooltip("選擇子彈圖片")]
-    public Sprite bullet;
+    [Header("子彈")]
+    public GameObject bullet;
 
-    [Header("子彈生成點"), Tooltip("選擇子彈生成位置")]
+    [Header("子彈生成點")]
     public Transform bulletPosition;
 
-    [Header("子彈速度"), Range(0,5000)]
+    [Header("子彈速度"), Range(0, 5000)]
     public int bulletSpeed = 800;
 
     [Header("開槍音效"), Tooltip("選擇開槍音效")]
     public AudioClip gunshotSound;
 
     [Header("血量"), Range(0, 200)]
-    public int hp = 100;
+    public float hp = 100;
 
- }
+    private AudioSource aud;
+    private Rigidbody2D rig;
+    private Animator ani;
+
+    #endregion
+
+    private void Move()
+    {
+        print("移動");
+    }
+
+    private void Jump()
+    {
+        print("跳躍");
+    }
+
+    private void Shot()
+    {
+        print("開槍");
+    }
+
+    private void Hurtanddie(int Hurt, string Die)
+    {
+        print("受傷:"+ Hurt);
+        print("死亡:"+ Die);
+    }
+
+    
+
+    private void Start()
+    {
+        Move();
+        Jump();
+        Shot();
+        Hurtanddie(120,"被石頭");       
+    }
+}
