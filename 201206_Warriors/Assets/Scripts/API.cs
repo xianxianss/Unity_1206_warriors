7fced76ac79647cfd5a5e6f58f99ed015dfb4724
xianxianss <75553503+xianxianss@users.noreply.github.com>
2021-01-03 13:04:21 +0800

    非靜態和切換場景

---
+++ 201206_Warriors/Assets/Scripts/API.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+
+public class API : MonoBehaviour
+{
+    public Transform traA;
+    public Transform tester;
+    public Camera cam;
+    public SpriteRenderer spt;
+    public GameObject objt;
+    private void Start()
+    {
+        print("座標:" + traA.position);
+
+        tester.position = new Vector3(2, 0, 0);
+
+        tester.Rotate(0, 0, 30);
+
+        print("攝影機數量:" + Camera.allCamerasCount);
+        cam.backgroundColor = new Color(0.3f, 6.6f, 8.1f);
+
+        print("圖片的顏色:" + spt.color);
+        print("物件的圖層:" + objt.layer);
+
+        spt.flipX = true;
+        objt.layer = 5;
+
+    }
+
+    private void Update()
+    {
+        tester.Rotate(0,0,30);
+        tester.Translate(0.5f, 0, 0,Space.World);
+    }
+}
---
+++ 201206_Warriors/Assets/Scripts/scenecontrol.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class scenecontrol : MonoBehaviour
+{
+    /// <summary>
+    /// 遊戲開始
+    /// </summary>
+   public void StartGame()
+    {
+        SceneManager.LoadScene("遊戲場景");
+    }
+
+    /// <summary>
+    /// 返回選單
+    /// </summary>
+   public void BackToMenu()
+    {
+        SceneManager.LoadScene("選單");
+    }
+
+
+    /// <summary>
+    /// 離開遊戲
+    /// </summary>
+   public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
